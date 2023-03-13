16bcae567da9dd86475b61aae13a20a41ea57d9b
Broderick <zerothesu@gmail.com>
2023-03-13 11:11:41 -0600

    connect to db

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,14 +2,24 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize('eCommerce_db', 'root', '32289216', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+})
+
+//test the connection to the database
+async function test(){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+test()
 
 module.exports = sequelize;
